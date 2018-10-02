e24418c269888316268ad40ea79ea6a83ea7a899
Manasi Sheth <sheth.manasi10@gmail.com>
2018-10-02 15:37:08 -0700

    Basic Select Queries - I

---
+++ Basic Select/Solution.sql
@@ -0,0 +1,11 @@
+/* Revising the Select Query - I */ 
+
+SELECT * 
+FROM city 
+WHERE population > 100000 AND CountryCode = "USA"; 
+
+/* Revising the Select Query - II */ 
+
+SELECT NAME 
+FROM city 
+WHERE population > 120000 AND CountryCode = "USA"; 
